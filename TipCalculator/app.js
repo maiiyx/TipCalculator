7dca6476fc8a03a2d12438c7ceb6e8e445000069
Willyanne Silva <110118532+maiiyx@users.noreply.github.com>
2023-05-29 18:04:51 -0300

    Add files via upload

---
+++ TipCalculator/app.js
@@ -0,0 +1,12 @@
+const valor = document.getElementById("valor");
+const btnCustom = document.getElementById("btnCustom");
+const pessoas = document.getElementById("pessoas");
+const outDivisao = document.getElementById("outDivisao");
+const btnReset = document.getElementById("btnReset");
+
+// botões das porcentagem
+const btn5 = document.getElementById("btn5");
+const btn10 = document.getElementById("btn10");
+const btn15 = document.getElementById("btn15");
+const btn25 = document.getElementById("btn25");
+const btn50 = document.getElementById("btn50");
